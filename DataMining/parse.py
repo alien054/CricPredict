e121bc0548f66be849255115db9163c07a2876ea
= <=>
2020-12-17 01:35:01 +0600

    :hammer: ADDED bowler data

--- DataMining/parse.py
+++ DataMining/parse.py
@@ -3,14 +3,8 @@ import os
 import csv
 from datetime import datetime
 
-path = "../../data/2005_male/"
+path = "../../../data/2005_male/"
 
-match_data = {'toss': "", 'venue': "", 'match_type': ""}
-team_data = {'batting_team': "", 'fielding_team': "",
-             'innings': "", 'over': "", 'ball': ""}
-batsman_data = {}
-bowler_data = {}
-class_label = {'outcome':""}
 
 files = [path + file for file in os.listdir(path) if 'yaml' in file]
 file = files[0]
@@ -18,8 +12,13 @@ file = files[0]
 index = 0
 
 for i in range(1):
-    with open(path+file, "r") as yamlfile:
+    with open(file, "r") as yamlfile:
         data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+        match_data = {'toss': "", 'venue': "", 'match_type': ""}
+        team_data = {'batting_team': "", 'fielding_team': "",
+                    'innings': "", 'over': "", 'ball': ""}
+        batsman_data = {}
+        bowler_data = {}
         
         team1 = data['info']['teams'][0]
         team2 = data['info']['teams'][1]
@@ -29,11 +28,7 @@ for i in range(1):
 
         innings = data['innings']
 
-        count = 0
         for inning in innings:
-            if count > 0:
-                break
-            count = 1
             innings_key = list(inning.keys())[0]
             team_data['innings'] = innings_key
             team_data['batting_team'] = inning[innings_key]['team']
@@ -44,6 +39,8 @@ for i in range(1):
             batsman_order = 0
             
             for delivery in deliveries:
+                class_label = {'outcome': ""}
+                
                 delivery_key = list(delivery.keys())[0]
                 over,ball = str(delivery_key).strip().split(".")
                 team_data['over'] = over
@@ -75,7 +72,7 @@ for i in range(1):
                         'is_out':0,
                         'balls_before_out':0,
                         }
-                    
+                
                 batsman_data[batsman_name]['non_strike'] = delivery[delivery_key]['non_striker']
                 batsman_run = delivery[delivery_key]['runs']['batsman']
                 batsman_data[batsman_name]['run_scored'] = batsman_data[batsman_name]['run_scored'] + batsman_run
@@ -100,8 +97,47 @@ for i in range(1):
                     batsman_data[batsman_name]['balls_before_out'] = batsman_data[batsman_name]['balls_faced']
                 
                 batsman_data['ball'] = delivery_key    
-                with open('test.yaml', 'a') as output:
-                    yaml.dump(batsman_data, output, default_flow_style=False)
+                
+                if bowler_name not in list(bowler_data.keys()):
+                    bowler_data[bowler_name] = {
+                        'team': team_data['fielding_team'],
+                        'innings': innings_key,
+                        'run_given': 0,
+                        'balls_delivered': 0,
+                        'boundary': 0,
+                        'dot_ball': 0,
+                        'single_double': 0,
+                        'five_haul': 0,
+                        'ten_haul': 0,
+                        'wicket': 0,
+                    }
+
+                bowler_run = delivery[delivery_key]['runs']['total']
+                bowler_data[bowler_name]['run_given'] = bowler_data[bowler_name]['run_given'] + bowler_run
+                bowler_data[bowler_name]['balls_delivered'] = bowler_data[bowler_name]['balls_delivered'] + 1
+
+                if bowler_run >= 4:
+                    bowler_data[bowler_name]['boundary'] = bowler_data[bowler_name]['boundary'] + 1
+                elif bowler_run == 0:
+                    bowler_data[bowler_name]['dot_ball'] = bowler_data[bowler_name]['dot_ball'] + 1
+                elif bowler_run <= 3:
+                    bowler_data[bowler_name]['single_double'] = bowler_data[bowler_name]['single_double'] + 1
+
+                if batsman_out:
+                    bowler_data[bowler_name]['wicket'] = bowler_data[bowler_name]['wicket'] + 1
+
+                bowler_total = bowler_data[bowler_name]['wicket']
+                if bowler_total >= 5 and bowler_total < 10:
+                    bowler_data[bowler_name]['five_haul'] = 1
+                elif bowler_total > 10:
+                    bowler_data[bowler_name]['five_haul'] = 0
+                    bowler_data[bowler_name]['ten_haul'] = 1
+
+                bowler_data['ball'] = delivery_key
+                with open('batsman.yaml', 'a') as batsman:
+                    yaml.dump(batsman_data, batsman, default_flow_style=False)
+                with open('bowler.yaml', 'a') as bowler:
+                    yaml.dump(bowler_data, bowler, default_flow_style=False)
                 print(over," ",ball," ",class_label['outcome'])
         print(match_data)
         
